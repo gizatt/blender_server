f3b2088fa94de7a0c9c1a47fc62014b5a7d4bd17
Greg Izatt <gizatt@mit.edu>
2021-06-07 22:47:56 -0400

    Documentation of the non-drake test.

--- test_blender_server.py
+++ test_blender_server.py
@@ -7,6 +7,17 @@ import time
 
 from blender_server_interface.blender_server_interface import BlenderServerInterface
 
+'''
+
+Demonstrates basic of the BlenderServerInterface by calling
+a handful of calls to render some YCB objects.
+
+To run, make sure you've already got the appropriate resources
+by running `setup_bionic.py`, and that you're already running
+`run_blender_server.sh` in another terminal. Then invoke this script with
+no arguments.
+'''
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         port = sys.argv[-1]
@@ -17,6 +28,13 @@ if __name__ == '__main__':
 
     bsi.send_remote_call("initialize_scene")
 
+    # Setup the background of the scene by loading an environment map.
+    env_map_path = "./data/env_maps/aerodynamics_workshop_4k.hdr"
+    bsi.send_remote_call(
+        "set_environment_map",
+        path=env_map_path)
+
+    # Register a metal material for the tabletop.
     metal26_path = "./data/test_pbr_mats/Metal26/Metal26"
     bsi.send_remote_call(
         "register_material",
@@ -24,7 +42,7 @@ if __name__ == '__main__':
         material_type="CC0_texture",
         path=metal26_path)
 
-
+    # Register the tabletop as a cube.
     bsi.send_remote_call(
         "register_object",
         name="obj_table",
@@ -33,7 +51,8 @@ if __name__ == '__main__':
         scale=[1.0, 1.0, 0.25],
         material="metal26")
 
-
+    # Register the YCB objects by first registering their material,
+    # and then registering a mesh object using that material.
     object_classes = [
             "./data/test_objs/ycb/004_sugar_box/google_16k/",
             "./data/test_objs/ycb/035_power_drill/google_16k/"
@@ -57,28 +76,25 @@ if __name__ == '__main__':
                 material="obj_%d_color" % i)
             objs.append(obj_name)
 
-
+    # Add a camera looking at the scene.
     bsi.send_remote_call(
         "register_camera",
         name="cam_1",
         location=[-.54, -.54, .12],
         quaternion=[-0.677, -0.604, 0.242, 0.365])
 
+    # Setup the camera resolution and output file format.
     bsi.send_remote_call(
         "configure_rendering",
         camera_name='cam_1',
         resolution=[1920, 1200],
         file_format="JPEG")
 
+    # For a bunch of frames, randomize the object
+    # poses and render the scene.
     plt.figure()
     for i in range(1000):
-        env_map_path = "./data/env_maps/aerodynamics_workshop_4k.hdr"
-        bsi.send_remote_call(
-            "set_environment_map",
-            path=env_map_path)
-
         bsi.send_remote_call("save_current_scene", path="./out/save.blend")
-
 
         for obj_tmp in objs:
             loc = [np.random.uniform(-0.4, 0.4),
